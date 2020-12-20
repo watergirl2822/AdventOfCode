a74d4788424f3c5b0e452c1f805e9fd4d0e97aaa
Melissa Clark <melissaclark4@mail.weber.edu>
2020-12-19 21:06:29 -0700

    Completed 2019 Day 1

---
+++ 2019/Day1/puzzle2.py
@@ -0,0 +1,21 @@
+f = open("input.txt")
+# print(f.read())
+totalFuelRequirements = 0
+def calc_Fuel(massAmount):
+    return (int(massAmount) // 3) - 2 # this is totalFuelRequirement for inputted mass of fuel
+
+
+while True:
+    mass = f.readline()
+    if not mass:
+        break
+    fuel = calc_Fuel(mass)
+    while fuel > 0:
+        totalFuelRequirements += fuel
+        fuel = calc_Fuel(fuel)
+
+f.close()
+print("Total Fuel Requirements:", totalFuelRequirements)
+
+
+# //this will round down automatically, you can't round up automatically
